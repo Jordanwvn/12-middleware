23e94d064e73baa8bf6c8ef5c2795cf2d89de528
Jordan Van Ness <jordanwvn@gmail.com>
2018-01-30 16:43:58 -0800

    set up files using yesterday's code with some of the sample code from today's demo.

---
+++ lab-jordan/__test__/unit-tests/error-handler.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const Note = require('../../lib/error-handler.js');
+require('jest');
+
+describe('ERROR HANDLER TEST', () => {
+  
+})
---
+++ lab-jordan/__test__/unit-tests/storage.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const Note = require('../../lib/storage.js');
+require('jest');
+
+describe('STORAGE TEST', () => {
+
+})
